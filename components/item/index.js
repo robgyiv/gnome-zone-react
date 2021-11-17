456409299039a574bac01e02f3af5f7571e6bde0
Robbie R <git@robbie.computer>
2021-11-17 21:34:12 +0000

    Don't show items that aren't GIFs
    For example, JS objects or XSS.

--- components/item/index.js
+++ components/item/index.js
@@ -17,44 +17,44 @@ export default function Item({ index, url, item, upvoteGif }) {
     setIsSubmitting(false);
   };
 
-  return (
-    <Flex flexDirection="column">
-      <Flex w="full" alignItems="center" justifyContent="center">
-        <Box
-          maxW="md"
-          w="xs"
-          mx="auto"
-          bg={useColorModeValue('white', 'gray.800')}
-          shadow="md"
-          rounded="md"
-        >
-          <Image h={48} w="full" fit="cover" mt={2} src={url} alt="Gnome GIF" />
+  if (item.gifLink.endsWith('.gif')) {
+    return (
+      <Flex flexDirection="column">
+        <Flex w="full" alignItems="center" justifyContent="center">
+          <Box
+            maxW="md"
+            w="xs"
+            mx="auto"
+            bg={useColorModeValue('white', 'gray.800')}
+            shadow="md"
+            rounded="md"
+          >
+            <Image h={48} w="full" fit="cover" mt={2} src={url} alt="Gnome GIF" />
 
-          <Flex alignItems="center" justifyContent="space-between" px={4} py={2} roundedBottom="lg">
-            <Text>{item.votes} votes</Text>
-            <Button
-              px={2}
-              py={1}
-              // bg="gray.800"
-              fontSize="xs"
-              // color="gray.900"
-              fontWeight="bold"
-              // rounded="md"
-              textTransform="uppercase"
-              // _hover={{
-              //   bg: 'gray.200',
-              // }}
-              // _focus={{
-              //   bg: 'gray.400',
-              // }}
-              rightIcon={isSubmitting && <CircularProgress isIndeterminate size="16px" />}
-              onClick={() => handleSubmit(index)}
+            <Flex
+              alignItems="center"
+              justifyContent="space-between"
+              px={4}
+              py={2}
+              roundedBottom="lg"
             >
-              Upgnome
-            </Button>
-          </Flex>
-        </Box>
+              <Text>{item.votes} votes</Text>
+              <Button
+                px={2}
+                py={1}
+                fontSize="xs"
+                fontWeight="bold"
+                textTransform="uppercase"
+                rightIcon={isSubmitting && <CircularProgress isIndeterminate size="16px" />}
+                onClick={() => handleSubmit(index)}
+              >
+                Upgnome
+              </Button>
+            </Flex>
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
-  );
+    );
+  }
+  return null;
 }
